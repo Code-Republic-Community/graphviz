71bb09d6007d1521606e71cded982ac2c538a655
Sveta-1999 <svetaseyranyan99@gmail.com>
2023-04-15 20:50:30 +0400

    change include guards

--- examples/golden_graph/edge.hpp
+++ examples/golden_graph/edge.hpp
@@ -1,10 +1,10 @@
-#ifndef EDGE_CLASS
-#define EDGE_CLASS
+#ifndef EXAMPLE_GOLDEN_GRAPH_EDGE_HPP
+#define EXAMOLE_GOLDEN_GRAPH_EDGE_HPP
 
 class Edge {
 public:
     Edge(int id_1, int id_2) : _id_1(id_1), _id_2(id_2) {}
-
+public:
     void set_id_1(int);
     int get_id_1() const;
 
@@ -16,4 +16,4 @@ private:
     int _id_2;
 };
 
-#endif //EDGE_CLASS
+#endif //EXAMPLE_GOLDEN_GRAPH_EDGE_HPP
