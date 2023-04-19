b3977074b3567df009d9205f67d6afec8cb60cf9
MovsesHovakimyan <moshovakimyan001@gmail.com>
2023-04-19 19:20:16 +0400

    By this  updating the class Node, i added getter and print functions.

--- examples/golden_graph/node.cpp
+++ examples/golden_graph/node.cpp
@@ -1,3 +1,25 @@
 #include "node.hpp"
 
 Node::Node(int id, int value) : m_id {id}, m_value {value} {}
+
+int Node::getID() const
+{
+	return m_id;
+}
+
+int Node::getValue() const
+{
+	return m_value;
+}
+
+std::vector<Edges*>& Node::getEdges() const
+{
+	return m_edges;
+}
+
+void Node::print()
+{
+	std::cout << "\nID: " << getID()
+		  << "\nValue: " << getValue()
+		  << "\nEdges: " << getEdges().print() << std::endl;
+}
