e29437c050973707795ca6dafc57a4c33fd22e0b
aramhovakimyan <hovakimyan.aram@mail.ru>
2023-04-21 12:44:20 +0400

    fix const qualifiers and add getID() method

--- examples/golden_graph/graph.cpp
+++ examples/golden_graph/graph.cpp
@@ -1,24 +1,21 @@
 #include "graph.hpp"
 
-const Node* Graph::get_node(int id) {
-    for (const auto& node: m_nodes) {
-        if (node->m_id == id) {
+const Node* Graph::getNode(int id) const {
+    for (const auto& node : m_nodes) {
+        if (node->getID() == id) {
             return node;
         }
     }
     return nullptr;
 }
 
-const std::vector<Node*>& getAllNodes() {
-    return m_nodes;
-}
+const std::vector<Node*>& Graph::getAllNodes() const { return m_nodes; }
 
 bool Graph::m_checkID(int id) {
-    for (const auto& node: m_nodes) {
-        if (node->m_id == id) {
+    for (const auto& node : m_nodes) {
+        if (node->getID() == id) {
             return true;
         }
     }
     return false;
 }
-
