ce5b9ff51374d6de2f4eef40a058fc9f175f1e19
aramhovakimyan <hovakimyan.aram@mail.ru>
2023-04-26 20:51:20 +0400

    create Graph::graphExport

--- examples/golden_graph/graph.hpp
+++ examples/golden_graph/graph.hpp
@@ -2,8 +2,12 @@
 #define EXAMPLES_GOLDEN_GRAPH_GRAPH_HPP
 
 #include "node.hpp"
+#include "edge.hpp"
+#include "../../libs/json/include/nlohmann/json.hpp"  // This relative path will be modified
 #include <iostream>
 #include <vector>
+
+using json = nlohmann::json;
 
 class Graph
 {
@@ -13,6 +17,7 @@ public:
     const Node* getNode(int id) const;
     const std::vector<Node*>& getAllNodes() const;
     void addEdge(int id1, int id2);
+    static void Graph::graphExport(const Graph* graph);
 private:
     bool m_checkID(int id);
 private:
