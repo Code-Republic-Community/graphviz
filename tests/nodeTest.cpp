b327ca41c4d02924958da35d8fb381c35ce980f4
MovsesHovakimyan <moshovakimyan001@gmail.com>
2023-04-29 17:55:06 +0400

    Node class tests part2

--- tests/nodeTest.cpp
+++ tests/nodeTest.cpp
@@ -11,7 +11,27 @@ const int testGetID(int id) {
     return nodeobj.getID();
 }
 
+const int testSetValue(int id) {
+    Node nodeobj(10,11);
+    nodeobj.setValue(id);
+    return nodeobj.getValue();
+}
+
+const int testDeleteEdge() {
+    Node nodeobj(12,13);
+    nodeobj.addEdge(1001,Direction::SOURCE);
+    nodeobj.addEdge(1002,Direction::SOURCE);
+    nodeobj.addEdge(1003,Direction::DESTINATION);
+    nodeobj.addEdge(1004,Direction::DESTINATION);
+    nodeobj.deleteEdge(1002);
+    nodeobj.deleteEdge(1004);
+
+    return nodeobj.getEdges().size();
+}
+
 TEST(test_template, first_test) {
     EXPECT_EQ(5, testValue(5));
     EXPECT_EQ(100, testGetID(100));
+    EXPECT_EQ(150, testSetValue(150));
+    EXPECT_EQ(2, testDeleteEdge());
 }
