7d6237ecebb11aced22b760ce896ab6074f88618
SarkisKhachatryan <sargis.khachatryan@picsart.com>
2023-05-01 19:38:19 +0400

    change deleteNode function so that it calls Graph::deleteEdge function

--- examples/golden_graph/graph.cpp
+++ examples/golden_graph/graph.cpp
@@ -1,7 +1,9 @@
 #include "graph.hpp"
 
+#include <vector>
+
 const Node* Graph::getNode(int id) const {
-    for (const auto& node: m_nodes) {
+    for (const auto& node : m_nodes) {
         if (node->getID() == id) {
             return node;
         }
@@ -9,12 +11,10 @@ const Node* Graph::getNode(int id) const {
     return nullptr;
 }
 
-const std::vector<Node*>& Graph::getAllNodes() const {
-    return m_nodes;
-}
+const std::vector<Node*>& Graph::getAllNodes() const { return m_nodes; }
 
 bool Graph::checkID(int id) const {
-    for (const auto& node: m_nodes) {
+    for (const auto& node : m_nodes) {
         if (node->getID() == id) {
             return true;
         }
@@ -22,38 +22,28 @@ bool Graph::checkID(int id) const {
     return false;
 }
 
-Node* Graph::m_getNode(int id) const
-{
-	for (auto& node : m_nodes)
-	{
-		if (node->getID() == id)
-		{
-			return node;
-		}
-	}
-	return nullptr;
+Node* Graph::m_getNode(int id) const {
+    for (auto& node : m_nodes) {
+        if (node->getID() == id) {
+            return node;
+        }
+    }
+    return nullptr;
 }
 
-void Graph::addEdge(int sourceID, int destinationID)
-{
-	if (checkID(sourceID) && checkID(destinationID))
-	{
-		m_getNode(sourceID)->addEdge(destinationID, Direction::DESTINATION);
-		m_getNode(destinationID)->addEdge(sourceID, Direction::SOURCE);
-	}
-	else
-	{
-		std::cout << "Invalid argument: " << std::endl;
-	}
+void Graph::addEdge(int sourceID, int destinationID) {
+    if (checkID(sourceID) && checkID(destinationID)) {
+        m_getNode(sourceID)->addEdge(destinationID, Direction::DESTINATION);
+        m_getNode(destinationID)->addEdge(sourceID, Direction::SOURCE);
+    } else {
+        std::cout << "Invalid argument: " << std::endl;
+    }
 }
 
-void Graph::printNodeProperties(int id) const
-{
-	m_getNode(id)->print();
-}
+void Graph::printNodeProperties(int id) const { m_getNode(id)->print(); }
 
 void Graph::deleteEdge(int id1, int id2) {
-	Node* node1 = m_getNode(id1);
+    Node* node1 = m_getNode(id1);
     Node* node2 = m_getNode(id2);
 
     if (!node1 || !node2) {
@@ -64,78 +54,78 @@ void Graph::deleteEdge(int id1, int id2) {
     for (auto it = node1->getEdges().begin(); it != node1->getEdges().end(); ++it) {
         Edge* edge = *it;
         if (edge->getID() == node2->getID()) {
-			node1->deleteEdge(id2);
-			break;
+            node1->deleteEdge(id2);
+            break;
         }
     }
 
     for (auto it = node2->getEdges().begin(); it != node2->getEdges().end(); ++it) {
         Edge* edge = *it;
         if (edge->getID() == node1->getID()) {
-			node2->deleteEdge(id1);
-			break;
-		}
+            node2->deleteEdge(id1);
+            break;
+        }
     }
 }
 
 void Graph::graphExport(const Graph* graph) {
-	// create integer to hold index of graph
-	static int indexOfGraph = 1;
-	
-	// create the jsonGraph object
-	json jsonGraph;
-	jsonGraph["node"] = json::array();
-	const auto nodes = graph->getAllNodes();
-	for (const auto& node : nodes) {
-		jsonGraph["node"].push_back(node->getID());
-	}
+    // create integer to hold index of graph
+    static int indexOfGraph = 1;
 
-	// create jsonNode objects for the graph
-	for (const auto& node : nodes) {
-		json jsonNode;
-		jsonNode["value"] = node->getValue();
-		jsonNode["neighbors"] = json::array();
+    // create the jsonGraph object
+    json jsonGraph;
+    jsonGraph["node"] = json::array();
+    const auto nodes = graph->getAllNodes();
+    for (const auto& node : nodes) {
+        jsonGraph["node"].push_back(node->getID());
+    }
 
-		// add each neighbor to the jsonNode object
-		const auto edges = node->getEdges();
-		for (const auto& edge : edges) {
-			jsonNode["neighbors"].push_back(edge->getID());
-		}
-		// add the jsonNode object to the graph object
-		jsonGraph[node->getID()] = jsonNode;
-	}
+    // create jsonNode objects for the graph
+    for (const auto& node : nodes) {
+        json jsonNode;
+        jsonNode["value"] = node->getValue();
+        jsonNode["neighbors"] = json::array();
 
-	// Add name of graph. Example graph_1, graph_2, ...
-	jsonGraph["name"] = "graph_" + std::to_string(indexOfGraph);
-	++indexOfGraph;
+        // add each neighbor to the jsonNode object
+        const auto edges = node->getEdges();
+        for (const auto& edge : edges) {
+            jsonNode["neighbors"].push_back(edge->getID());
+        }
+        // add the jsonNode object to the graph object
+        jsonGraph[node->getID()] = jsonNode;
+    }
+
+    // Add name of graph. Example graph_1, graph_2, ...
+    jsonGraph["name"] = "graph_" + std::to_string(indexOfGraph);
+    ++indexOfGraph;
 
     // write the JSON object to a file
     std::ofstream file("export.json", std::ios::app);
-    file << jsonGraph.dump(4); // pretty print with 4 spaces
+    file << jsonGraph.dump(4);  // pretty print with 4 spaces
     file.close();
 }
 
-void Graph::addNode(int id, int value)
-{
-	if (checkID(id))
-	{
-		std::cout << "Your have such node: Please change your node id:" << std::endl;
-		return;
-	}
-	m_nodes.push_back(new Node(id, value));
+void Graph::addNode(int id, int value) {
+    if (checkID(id)) {
+        std::cout << "Your have such node: Please change your node id:" << std::endl;
+        return;
+    }
+    m_nodes.push_back(new Node(id, value));
 }
 
-
-// void Graph::deleteNode(int id)
-// {
-// 	if (!checkID(id))
-// 	{
-// 		std::cout << "wrong id";
-// 		return ;
-// 	}
-// 	Node* node = get_node(id);
-// 	for (int i = 0; i < node -> m_edges.size(); ++i)
-// 	{
-// 		node -> m_edges[i] -> deleteEdge(m_edges[i].get_ID());
-// 	}
-// }
+void Graph::deleteNode(int id) {
+    if (!checkID(id)) {
+        std::cout << "wrong id";
+        return;
+    }
+    Node* node = m_getNode(id);
+    auto edges = node->getEdges();
+    std::vector<int> deleteIDs;
+    for (int i = 0; i < node->getEdges().size(); ++i) {
+        deleteIDs.emplace_back(edges[i]->getID());
+    }
+    for (int i = 0; i < deleteIDs.size(); ++i) {
+        deleteEdge(id, deleteIDs[i]);
+    }
+    delete node;
+}
